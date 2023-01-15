ade3dbbc16851efa2902c70b176ef63bda70ffa2
Frisy Novel <98008631+freezyyyyyy@users.noreply.github.com>
2023-01-15 12:34:58 +0700

    latihan oop

--- cuba.js
+++ cuba.js
@@ -1,4 +1,32 @@
-let tercurry = 'scrapdata'
+// let test1 = "test1 git hub";
+// let haha = 'test 2 git hub';
 
-console.log('scrapdata')
+// for (let i = 0; i < 9; i++) {
+//     console.log(test1);
+//     console.log(haha);
+// }
 
+class character{
+    constructor(hp, mp, energy){
+        this.hp = hp;
+        this.mp = mp;
+        this.energy = energy;
+    }
+
+    getPower(){
+        return this.hp + this.mp;
+    }
+
+    getEnergyPersen(){
+        return this.energy * 100;
+    }
+}
+
+const kazuha = new character(80, 40, 100);
+
+let hello = document.getElementById('konteks');
+hello.innerText = `HP : ${kazuha.getPower()}`;
+
+
+console.log(kazuha.getPower());
+console.log(kazuha.getEnergyPersen());
